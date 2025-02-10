94ffbb5c5cf135024cbd65c7c6d7c996dd1d2dbb
VicoLamico <1312core@gmail.com>
2025-02-10 07:43:43 +0100

    init

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import Aurelia, { AppTask } from 'aurelia';
+import { MyApp } from './my-app';
+import { AllConfiguration } from '@aurelia-mdc-web/all';
+Aurelia
+.register(AllConfiguration)
+  .app(MyApp)
+  .start();
