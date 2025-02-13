0f34ab92c01d27335e25d7352bb39edf127e544e
VicoLamico <1312core@gmail.com>
2025-02-13 13:01:30 +0100

    prepare backend

---
+++ frontend/src/main.ts
@@ -0,0 +1,18 @@
+import Aurelia, { AppTask } from 'aurelia';
+import { MyApp } from './my-app';
+import { AllConfiguration } from '@aurelia-mdc-web/all';
+import { RouterConfiguration } from '@aurelia/router';
+import { FourOhFour } from './resources/fourohfour-page';
+import { Header } from './views/header/header';
+
+Aurelia
+.register(
+  Header,
+  AllConfiguration,
+  RouterConfiguration.customize({useUrlFragmentHash:false, basePath:'/'}), 
+  FourOhFour,
+
+  )
+
+  .app(MyApp)
+  .start();
